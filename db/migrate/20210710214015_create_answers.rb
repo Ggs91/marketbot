50693eda2823e6495fb64a2d657378045b659c53
Ggs91 <georges_atalla@hotmail.fr>
2021-07-11 00:06:22 +0200

    implements question questionnaire & answer model

---
+++ db/migrate/20210710214015_create_answers.rb
@@ -0,0 +1,10 @@
+class CreateAnswers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :answers do |t|
+      t.belongs_to :question, foreign_key: true, index: true
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
