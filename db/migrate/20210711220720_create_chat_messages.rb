fdb8ade7751a13dbf5335c560b68f502a39982c2
Ggs91 <georges_atalla@hotmail.fr>
2021-07-12 14:11:03 +0200

    add chatroom reference

--- db/migrate/20210711220720_create_chat_messages.rb
+++ db/migrate/20210711220720_create_chat_messages.rb
@@ -2,6 +2,7 @@ class CreateChatMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :chat_messages do |t|
       t.references :sendable, polymorphic: true
+      t.belongs_to :chat_room, index: true
       t.timestamps
     end
   end
