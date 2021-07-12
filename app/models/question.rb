61a65b43d48d4851065ebeb99b57791ae1c74e19
Ggs91 <georges_atalla@hotmail.fr>
2021-07-12 14:47:52 +0200

    create message and question association

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :questionnaire
+  belongs_to :message, optional: true
   has_many :answers, dependent: :destroy
   accepts_nested_attributes_for :answers, allow_destroy: true
 
