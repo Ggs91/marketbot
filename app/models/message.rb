46966b5763eafc65f03e274e6f7c5d245f0d8414
Ggs91 <georges_atalla@hotmail.fr>
2021-07-13 22:54:39 +0200

    add enum message_type

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :chatroom
   belongs_to :sender, polymorphic: true
+
+  enum message_type: { question: 0, answer: 1, introduction: 2 }
+
+  def sender_type
+    self.sender.is_a?(User) ? 'user' : 'bot'
+  end
 end
