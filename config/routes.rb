0075a0ca588d2a58026b4154c93bf935be80fda1
Ggs91 <georges_atalla@hotmail.fr>
2021-07-13 14:55:28 +0200

    remove unnecessary chatrooms routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'chatrooms#index'
-  resources :chatrooms, except: [:index]
+  resources :chatrooms, only: [:show, :destroy, :create]
   resources :questionnaires
 end
