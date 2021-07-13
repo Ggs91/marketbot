18159596405960a1223ab7e203a0b74896d1cbb5
Ggs91 <georges_atalla@hotmail.fr>
2021-07-13 18:24:27 +0200

    implement send_introduction_message

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,4 +1,17 @@
 class Bot < ApplicationRecord
   has_many :messages, as: :sender, dependent: :destroy
   has_one :chatroom, dependent: :destroy
+
+  def send_introduction_message
+    self.messages.create(
+      chatroom: self.chatroom,
+      content: introduction_message
+    )
+  end
+
+  private
+
+  def introduction_message
+    "Hello"
+  end
 end
