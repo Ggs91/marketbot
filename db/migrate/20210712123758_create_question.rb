76eb600c96b5a8e6dce6a81a85dd5b8925329bdf
Ggs91 <georges_atalla@hotmail.fr>
2021-07-14 22:00:15 +0200

    remove message references

--- db/migrate/20210712123758_create_question.rb
+++ db/migrate/20210712123758_create_question.rb
@@ -2,7 +2,6 @@ class CreateQuestion < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
       t.references :questionnaire, foreign_key: true, index: true
-      t.references :messages, foreign_key: true, index: true
       t.string :name
       t.integer :question_type
 
