02d88a3792ae3f8666c019c27687752ca3267fbe
Ggs91 <georges_atalla@hotmail.fr>
2021-07-14 17:03:19 +0200

    implement current_question instance variable

--- app/models/completion.rb
+++ app/models/completion.rb
@@ -2,4 +2,14 @@ class Completion < ApplicationRecord
   belongs_to :questionnaire
   has_many :answers
   accepts_nested_attributes_for :answers, allow_destroy: true
+
+  attr_writer :current_question
+
+  def current_question
+    @current_question || 1
+  end
+
+  def final_question
+    self.questionnaire.questions_count
+  end
 end
