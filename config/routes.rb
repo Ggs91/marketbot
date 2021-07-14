d4483794b9cab9bd8af1d11794e0a868b44565b9
Ggs91 <georges_atalla@hotmail.fr>
2021-07-14 17:05:58 +0200

    nest messages under chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'chatrooms#index'
-  resources :chatrooms, only: [:show, :destroy, :create]
+  resources :chatrooms, only: [:show, :destroy, :create] do
+    resources :messages, only: [:create], module: :chatrooms
+  end
   resources :questionnaires do
     resources :completions, only: [:create, :show, :destroy], module: :questionnaires
   end
