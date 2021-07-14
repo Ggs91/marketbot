139c4360dff46d8e7670ff9644f11d3239d96a58
Ggs91 <georges_atalla@hotmail.fr>
2021-07-14 17:04:27 +0200

    initiate completion creation

--- app/controllers/questionnaires/completions_controller.rb
+++ app/controllers/questionnaires/completions_controller.rb
@@ -1,7 +1,14 @@
 class Questionnaires::CompletionsController < ApplicationController
+
+  before_action :set_questionnaire, only: [:update]
+  before_action :set_chatroom, only: [:create, :update]
+
   def create
+    user_reply
+
     @completion = Completion.new(completion_params)
     if @completion.save
+      render 'chatrooms/show'
       # session[:completion_id] = @completion.id
     else
 
@@ -19,5 +26,17 @@ class Questionnaires::CompletionsController < ApplicationController
       :questionnaire_id,
       answers_attributes: [:id, :name]
     )
+  end
+
+  def set_questionnaire
+    @questionnaire = Questionnaire.find(params[:questionnaire_id])
+  end
+
+  def set_chatroom
+    @chatroom = Chatroom.find(params[:chatroom_id])
+  end
+
+  def user_reply
+    current_user.send_message(@chatroom, 'Start')
   end
 end
