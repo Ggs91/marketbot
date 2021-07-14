de6c0890502fc0fdd77ab4a3a2f2797f64a821ee
Ggs91 <georges_atalla@hotmail.fr>
2021-07-14 17:03:48 +0200

    implement #send_message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   has_many :messages, as: :sender, dependent: :destroy
   has_many :chatrooms, dependent: :destroy
+
+  def send_message(chatroom, content, message_type='reply')
+    self.messages.create(
+      chatroom: chatroom,
+      message_type: message_type,
+      content: content
+    )
+  end
 end
