f9a9231f72e709744908cc1576465428419f8eee
Ggs91 <georges_atalla@hotmail.fr>
2021-07-14 21:57:07 +0200

    implement #listent

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -2,19 +2,28 @@ class Bot < ApplicationRecord
   has_many :messages, as: :sender, dependent: :destroy
   has_one :chatroom, dependent: :destroy
 
-  # def listen(message_ou_autre)
-  #   if
-  #   elsif
-  #     self.send_message()
-  #   else
-  #   end
-  # end
+  def listen(record)
+    if record.is_a? Completion
+      self.process_completion(record)
+    elsif
+      self.send_message()
+    else
+    end
+  end
 
-  def send_message(content, message_type)
+  def process_completion(completion)
+    if completion.has_one_last_question_to_answer?
+      self.send_message('One last question', 'question', completion.)
+      completion.current_question = nil
+    end
+  end
+
+  def send_message(content, message_type, question=nil)
     self.messages.create(
       chatroom: self.chatroom,
       message_type: message_type,
-      content: content
+      content: content,
+      question: question
     )
   end
 
