7d7efeff8d22097a65b135817e67c5f8926305a0
Ggs91 <georges_atalla@hotmail.fr>
2021-07-15 23:23:48 +0200

    add chatroom ref on completions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_180542) do
+ActiveRecord::Schema.define(version: 2021_07_15_180345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_07_14_180542) do
     t.bigint "questionnaire_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "chatroom_id"
+    t.index ["chatroom_id"], name: "index_completions_on_chatroom_id"
     t.index ["questionnaire_id"], name: "index_completions_on_questionnaire_id"
   end
 
@@ -90,6 +92,7 @@ ActiveRecord::Schema.define(version: 2021_07_14_180542) do
   add_foreign_key "chatrooms", "bots"
   add_foreign_key "chatrooms", "questionnaires"
   add_foreign_key "chatrooms", "users"
+  add_foreign_key "completions", "chatrooms"
   add_foreign_key "completions", "questionnaires"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "questions"
