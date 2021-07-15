f651e53b26299cc691e90c4616a504071b10af7b
Ggs91 <georges_atalla@hotmail.fr>
2021-07-15 23:24:34 +0200

    typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,73 +1,76 @@
 require 'activerecord-reset-pk-sequence'
 
+Completion.destroy_all
+Message.destroy_all
 User.destroy_all
 Chatroom.destroy_all
 Bot.destroy_all
 Answer.destroy_all
-Completion.destroy_all
 Question.destroy_all
 Questionnaire.destroy_all
 
-Chatroom.reset_pk_sequence
+Completion.reset_pk_sequence
+Message.reset_pk_sequence
 User.reset_pk_sequence
+Chatroom.reset_pk_sequence
 Bot.reset_pk_sequence
 Answer.reset_pk_sequence
-Completion.reset_pk_sequence
 Question.reset_pk_sequence
 Questionnaire.reset_pk_sequence
 
 
 # Questionnaire 1
 questionnaire_1 = Questionnaire.create!(
-  presentation: 'We would like to ask you a few questions about your last shopping session'
+  presentation: 'We would like to ask you a few questions about your last shopping session.'
 )
 
 q1 = questionnaire_1.questions.create!(
-  name: 'How many items did you buy ?',
+  name: 'How many items did you buy?',
   question_type: 0
 )
 
 q2 = questionnaire_1.questions.create!(
-  name: 'Which items of the list below did you buy ?',
+  name: 'Which items of the list below did you buy?',
   question_type: 1
 )
 
 q3 = questionnaire_1.questions.create!(
-  name: 'Are you happy with what you bought ?',
+  name: 'Are you happy with what you bought?',
   question_type: 2
 )
 
 q1.answers.create!([
-  {name: 'Shoes'},
-  {name: 'Pants'},
-  {name: 'Sunglasses'}
-])
-
-q2.answers.create!([
   {name: '0'},
   {name: '1 to 3'},
   {name: '4 or more'}
   ])
 
+q2.answers.create!([
+  {name: 'Shoes'},
+  {name: 'Pants'},
+  {name: 'Sunglasses'}
+])
+
+
 q3.answers.create!
 
 # Questionnaire 2
 questionnaire_2 = Questionnaire.create!(
-  presentation: 'We would like to ask you a few questions about your shopping habits'
+  presentation: 'We would like to ask you a few questions about your shopping habits.'
 )
 
 q4 = questionnaire_2.questions.create!(
-  name: 'How often do you do shopping per year ?',
+  name: 'How often do you go shopping in a  year?',
   question_type: 0
 )
 
 q5 = questionnaire_2.questions.create!(
-  name: 'Where do you usually go shopping ?',
+  name: 'Where do you usually go shopping?',
   question_type: 1
 )
 
 q6 = questionnaire_2.questions.create!(
-  name: 'Do you schedule your shopping sessions ?',
+  name: 'Do you schedule your shopping sessions?',
   question_type: 2
 )
 
