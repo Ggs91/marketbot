cb1c84c691cad988e9a1dcd1cc4d39b8c66bac27
Ggs91 <georges_atalla@hotmail.fr>
2021-07-16 18:35:24 +0200

    implement #is_at_first_question? & is_at_last_question?

--- app/models/completion.rb
+++ app/models/completion.rb
@@ -1,15 +1,19 @@
 class Completion < ApplicationRecord
+  attr_writer :current_question_index
+
   belongs_to :questionnaire
   has_many :answers
   belongs_to :chatroom
 
-  attr_writer :current_question
-
-  def current_question
-    @current_question || 1
+  def current_question_index
+    @current_question_index || 0
   end
 
-  def has_one_last_question_to_answer?
-    self.questionnaire.questions_count == @current_question
+  def is_at_first_question?
+    self.current_question_index == 0
+  end
+
+  def is_at_last_question?
+    self.questionnaire.questions_count == current_question_index + 1
   end
 end
